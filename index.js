1e196e4cb974a835c8619a05fd93475c00873dee
Thomas Aleksander Flier <thomas.flier@no.experis.com>
2024-09-23 15:40:41 +0200

    almost done

--- index.js
+++ index.js
@@ -53,3 +53,75 @@ const state = {
   ],
   cart: []
 };
+
+
+function renderItems() {
+  const storeList = document.querySelector('.store--item-list')
+  state.items.forEach(item => {
+    const li = document.createElement('li')
+    li.innerHTML = `
+      <div class="store--item-icon">
+        <img src="assets/icons/${item.id}.svg" alt="${item.name}" />
+      </div>
+      <button>Add item to cart</button>
+    `;
+
+    li.querySelector('button').addEventListener('click', () => addToCart(item) + console.log("satabn1"))
+    storeList.appendChild(li)
+  })  
+}
+
+function addToCart(item) {
+  const cartItem = state.cart.find(i => i.id === item.id)
+
+  if(cartItem){
+    cartItem.quantity++
+  } else {
+    state.cart.push({...item, quantity: 1})
+  }
+  showCart()
+}
+
+function showCart() {
+  const cartList = document.querySelector('.cart--item-list')
+  cartList.innerHTML = ''
+  state.cart.forEach(item => {
+    const li = document.createElement('li')
+    li.innerHTML = `
+      
+        <img src="assets/icons/${item.id}.svg" alt="${item.name}" />
+        <p>${item.name}</p>
+        <button class="quantity-btn remove-btn center">-</button>
+        <span class="quantity-text remove-btn center">${item.quantity}</span>
+        <button class="quantity-btn add-btn center">+</button>
+      
+      
+    `;
+
+    li.querySelector('.add-btn').addEventListener('click', () => itemQuantity(item, 1))
+    li.querySelector('.remove-btn').addEventListener('click', () => itemQuantity(item, -1))
+
+    cartList.appendChild(li)
+  })
+  console.log(cart)
+
+  totalCost()
+}
+
+function itemQuantity(item, valueChange) {
+  item.quantity += valueChange
+  if (item.quantity === 0){
+    state.cart.pop(i => i.id === item.id)
+  }
+  showCart()
+}
+
+function totalCost() {
+  const totalElement = document.querySelector('.total-number')
+  const total = cart.state.reduce((acc, item) => acc + (item.price * item.quantity), 0)
+  totalElement.textContent = `${Math.round(total)}`
+  
+}
+
+renderItems()
+//showCart()
